e23007fb8ebe7c284e717623ed10e7639012572d
wangbenchi66 <69945864@qq.com>
2024-02-29 09:42:54 +0800

    已修改        docker-compose.sh 已修改        docker-compose.yml

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #current_time=$(date +"%Y%m%d%H%M%S")
-current_time=$(git log -n 1 --pretty=format:%H)
-echo ------------------------------------------
-echo VERSION=$current_time > .env
-echo ------------------------------------------
-echo 设置版本:$current_time
-echo ------------------------------------------
+# current_time=$(git log -n 1 --pretty=format:%H)
+# echo ------------------------------------------
+# echo VERSION=$current_time > .env
+# echo ------------------------------------------
+# echo 设置版本:$current_time
+# echo ------------------------------------------
 git checkout -- .
 git pull
-git log -n 5
+git log -n 1
 echo ------------------------------------------
 echo git拉取成功
 echo ------------------------------------------
@@ -18,4 +18,3 @@ docker-compose up -d
 echo ------------------------------------------
 echo 编译成功
 echo ------------------------------------------
-chmod +x docker-compose.sh
